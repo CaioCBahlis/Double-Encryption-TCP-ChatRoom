00d1a9e9d67a523ebd767f673bc0f3c4d9df8c19
CaioCBahlis <104174800+CaioCBahlis@users.noreply.github.com>
2024-01-08 23:49:10 -0300

    Client.py

--- Client.py
+++ Client.py
@@ -2,19 +2,21 @@ import socket
 import threading
 import EncryptMe
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, AES
 
 Nickname = str(input("What is your Nickname?"))
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 65534))
+client.connect(("127.0.0.1", 65511))
 
 def EncryptME(message):
-    cipher = PKCS1_OAEP.new(SV_Public_Key)
-    ciphertext = cipher.encrypt(message)
-    return ciphertext
-    
+    cipher = AES.new(Aes_Key, AES.MODE_EAX)
+    message, tag = cipher.encrypt_and_digest(message)
+    nonce = cipher.nonce
+    print(f'{type(cipher), type(message), type(nonce)}')
+    AESPAYLOAD = tag+nonce+bytes(message)
+    return AESPAYLOAD
 def receive():
     while True:
         try:
@@ -31,11 +33,13 @@ def receive():
 def write():
     while True:
         message = f"{Nickname}: {input()}"
+        print(message)
         Encmessage = EncryptME(message.encode("utf-8"))
+        print(Encmessage)
         client.send(Encmessage)
 
 def RSASetup():
-    Serialized_Sv_PublicKey = client.recv(2048)
+    Serialized_Sv_PublicKey = client.recv(4096)
     SV_Public_Key = RSA.import_key(Serialized_Sv_PublicKey)
     print(f"Server Serialized PKey: {Serialized_Sv_PublicKey} and not Serialized {SV_Public_Key}  ")
     key = RSA.generate(2048)
@@ -47,10 +51,24 @@ def RSASetup():
     except:
         print("Encryption has failed")
         client.close()
-    return SV_Public_Key
+
+    print("AES")
+    AES_KEY = AESSetup(key)
+
+    return AES_KEY
+
+def AESSetup(key):
+    AESKey = client.recv(2048)
+    print(AESKey)
+
+    countercipher = PKCS1_OAEP.new(key)
+    AESKey = countercipher.decrypt(AESKey)
 
 
-SV_Public_Key = RSASetup()
+    return AESKey
+
+
+Aes_Key = RSASetup()
 
 
 rcvthread = threading.Thread(target=receive)
